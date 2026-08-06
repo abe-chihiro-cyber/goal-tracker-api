09a81f12b5e02a59a027033c8ad302eb5a7ec761
chihiroA <abe@codeunit.co.jp>
2026-08-06 11:14:46 +0900

    Initial Spring Boot project

---
+++ src/test/java/com/chihiro/goaltrackerapi/GoalTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chihiro.goaltrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
